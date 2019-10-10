b49057ef22a198f7da5d514b3fea42f9530c859c
Zee Chen <zeyuan.chen1126@gmail.com>
2019-10-09 19:53:07 -0700

    feat: read json file

---
+++ Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterInfo
+{
+    private string name;
+    private int group;
+    private Color color;
+    private float x;
+    private float y;
+    private float z;
+
+    public static CharacterInfo CreateFromJSON(string jsonString)
+    {
+        // creates CharacterInfo object 
+        Debug.Log(JsonUtility.FromJson<CharacterInfo>(jsonString));
+        return JsonUtility.FromJson<CharacterInfo>(jsonString);
+    }
+    
+}
---
+++ Assets/Scripts/LoadData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadData : MonoBehaviour
+{
+    private string jsonFile = "lesmis-3d.json";
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadJsonData();
+    }
+
+    // Update is called once per frame
+    private void LoadJsonData()
+    {
+        string fPath = Path.Combine(Application.dataPath, jsonFile);
+        
+        if (File.Exists(fPath))
+        {
+            
+            // read JSON file
+            string JSONData = File.ReadAllText(fPath);
+            // create Character Info objects
+            CharacterInfo loadedInfo = CharacterInfo.CreateFromJSON(JSONData);
+            
+            // create relationship objects
+        }
+        else
+        {
+            Debug.Log("File does not exist");
+        }
+    }
+}
